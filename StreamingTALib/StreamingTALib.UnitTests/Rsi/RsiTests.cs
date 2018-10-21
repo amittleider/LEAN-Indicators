3ab6a132ab4637cf12a2d32c7669270d6c72cc51
Andrew Mittleider <amittleider@gmail.com>
2018-10-21 12:00:33 +0200

    RSI

---
+++ StreamingTALib/StreamingTALib.UnitTests/Rsi/RsiTests.cs
@@ -0,0 +1,62 @@
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace StreamingTALib.UnitTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using FluentAssertions;
+    using Xunit;
+
+    /// <summary>
+    /// Test the RSI calcuations
+    /// </summary>
+    public class RsiTests
+    {
+        /// <summary>
+        /// Test that an RSI25 is properly initialized and streaming values are computed properly
+        /// </summary>
+        [Fact]
+        public void Rsi25_Should_ProduceCorrectResult()
+        {
+            List<double> performanceData = GetPerformanceDemoData();
+
+            int period = 25;
+            int lookback = RsiInitializer.GetLookback(period);
+
+            List<double> rsiCalculationData = performanceData.TakeLast(lookback + 4).ToList();
+            List<double> rsiInitializerData = rsiCalculationData.Take(lookback).ToList();
+            List<double> streamingData = rsiCalculationData.Skip(lookback).ToList();
+
+            RsiInitializer rsiInitializer = new RsiInitializer(period);
+            Rsi rsi = rsiInitializer.InitalizeWithData(rsiInitializerData);
+
+            rsi.CalculateRsiWithNewPrice(streamingData[0]).RsiValue.Should().BeApproximately(51.6190, 0.0001);
+            rsi.CalculateRsiWithNewPrice(streamingData[1]).RsiValue.Should().BeApproximately(52.8735, 0.0001);
+            rsi.CalculateRsiWithNewPrice(streamingData[2]).RsiValue.Should().BeApproximately(49.8797, 0.0001);
+            rsi.CalculateRsiWithNewPrice(streamingData[3]).RsiValue.Should().BeApproximately(51.3154, 0.0001);
+        }
+
+        private static List<double> GetPerformanceDemoData()
+        {
+            string csv_location = @"IB-NQ-Future3M-20-Globex.csv";
+            List<double> closes = new List<double>();
+            List<DateTime> dateTimes = new List<DateTime>();
+
+            using (var reader = new StreamReader(csv_location))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    double close = double.Parse(values[4]);
+                    closes.Add(close);
+                }
+            }
+
+            return closes;
+        }
+    }
+}
---
+++ StreamingTALib/StreamingTALib/Rsi/RsiInitializer.cs
@@ -0,0 +1,62 @@
+﻿// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace StreamingTALib
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// This class initializes an RSI object and RSI state object properly.
+    /// </summary>
+    public class RsiInitializer
+    {
+        private int period;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RsiInitializer"/> class.
+        /// </summary>
+        /// <param name="period">The RSI period</param>
+        public RsiInitializer(int period)
+        {
+            this.period = period;
+        }
+
+        /// <summary>
+        /// Gets the lookback value. The lookback is the minimum number of data points needed to initialize an RSI.
+        /// The rsi lookback `l = 7x + 200` is proven to be stable at all points.
+        /// An RSI with a lower lookback will be unstable across different periods of the same data set.
+        /// </summary>
+        /// <param name="period">The RSI period</param>
+        /// <returns>The lookback</returns>
+        public static int GetLookback(int period)
+        {
+            int lookback = (7 * period) + 200;
+            return lookback;
+        }
+
+        /// <summary>
+        /// Initialize the RSI and RSI state objects with a data array.
+        /// This computation time is the same as running a TALib.Rsi(prices, out rsi)
+        /// </summary>
+        /// <param name="prices">The prices</param>
+        /// <returns>An RSI object</returns>
+        public Rsi InitalizeWithData(List<double> prices)
+        {
+            if (prices.Count < RsiInitializer.GetLookback(this.period))
+            {
+                throw new Exception("Cannot initialize the RSI with this period. Not enough values.");
+            }
+
+            RsiState rsiState = null;
+            Rsi rsi = new Rsi(this.period);
+            rsi.RsiState.PreviousPrice = prices.First();
+            for (int i = 1; i < prices.Count; i++)
+            {
+                rsiState = rsi.CalculateRsiWithNewPrice(prices[i]);
+            }
+
+            return rsi;
+        }
+    }
+}
